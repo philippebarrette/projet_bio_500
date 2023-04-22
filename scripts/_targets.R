0578382fec08f469523d045c1dd15471a7be1481
mcarseneau <arsm1403@usherbrooke.ca>
2023-04-21 21:02:52 -0400

    Modif du code

--- scripts/_targets.R
+++ scripts/_targets.R
@@ -18,31 +18,30 @@ source("fonctions_target.R")
 tar_option_set(packages = c("RSQLite", "tidyverse","MASS", "igraph", "rmarkdown",
                             "ggplot2", "rticles","igraph","RColorBrewer","viridis"))
 list(
-#Lecture des données
+#Lecture des donnees
   tar_target(tab_collaboration,read.csv("datatbl_collaborations.csv", sep=";")),
   tar_target(tab_cours,read.csv("datatbl_cours.csv", sep=";")),
   tar_target(tab_etudiants,read.csv("datatbl_etudiants.csv", sep=";")),
-#Connection à SQL
+#Connection a SQL
   tar_target(con,f_connect()),
-#Création des tables SQL
+#Creation des tables SQL
   tar_target(tables,f_creation_table(con,tab_collaboration,tab_cours,tab_etudiants)),
-#Requête SQL hist collab
+#Requete SQL hist collab
   tar_target(requete_hist1,f_requete1() ),
-#Création hist collab
+#Creation hist collab
   tar_target(hist_collab,f_hist_collab(requete_hist1)),
-#Requête SQL hist cours
+#Requete SQL hist cours
   tar_target(requete_hist2,f_requete2()),   
-#Création hist cours
+#Creation hist cours
   tar_target(hist_cours,f_hist_cours(requete_hist2)),
-#Création figure réseau de collab
-  tar_target(fig_reseau,f_reseau())
-)
-
+#Creation figure reseau de collab
+  tar_target(fig_reseau,f_reseau()),
 #Creation Markdown
   tar_render(rapport,"rapport.Rmd")
+)
 
 
-source("targets.R")
+source("_targets.R")
 tar_glimpse()
 tar_make()
 tar_visnetwork()
